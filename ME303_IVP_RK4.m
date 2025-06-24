a74bca77d91f4d1c6021e8a2bd6aff92958d267a
Alex Trinh <112913946+alexkltrinh@users.noreply.github.com>
2025-06-24 18:43:04 -0400

    Create ME303_IVP_RK4.m

---
+++ ME303_IVP_RK4.m
@@ -0,0 +1,18 @@
+% Generic IVP solver
+function [t, y] = solveIVP(f, tspan, y0, h, solver)
+    t = tspan(1):h:tspan(2);
+    y = zeros(length(y0), length(t));
+    y(:,1) = y0;
+    for n = 1:length(t) - 1
+        y(:,n+1) = solver(f, t(n), y(:,n), h);
+    end
+end
+
+% Runge-Kutta 4th order method
+function ynew = rk4(f, t, y, h)
+    k1 = f(t, y);
+    k2 = f(t + 0.5 * h, y + 0.5 * h * k1);
+    k3 = f(t + 0.5 * h, y + 0.5 * h * k2);
+    k4 = f(t + h, y + h * k3);
+    ynew = y + (h / 6) * (k1 + 2*k2 + 2*k3 + k4);
+end
